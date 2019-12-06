53e101790243d6f65c18307f751e058006c74898
mgtm <m.g.t.m.prog@gmail.com>
2019-12-06 14:24:28 +0200

    Added Documentation

--- coral/coral.py
+++ coral/coral.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
+import math
 
 image = cv2.imread("Capture.PNG")
 
-abstracted_coral = np.zeros((424,387,3), np.uint8)
+abstracted_coral = np.zeros((424,387,3), np.uint8)              # Empty image to draw on
 
-hsv= cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+#########################################    Color Mask    ###########################################
+hsv= cv2.cvtColor(image,cv2.COLOR_BGR2HSV)                      
 lower_pink= np.array([150,80,50])
 higher_pink = np.array([170,255,255])
 pink_mask= cv2.inRange(hsv,lower_pink,higher_pink)
@@ -13,16 +15,34 @@ pink_mask= cv2.inRange(hsv,lower_pink,higher_pink)
 lower_white= np.array([0,0,210])
 higher_white = np.array([255,255,255])
 white_mask= cv2.inRange(hsv,lower_white,higher_white)
+                                                                                                    
+mask = pink_mask + white_mask                                                                       
+######################################################################################################
 
-mask = pink_mask + white_mask
+################################ Another approch #####################################################
 
+x, y, _ = image.shape
+print(math.ceil(x / 25))
+print(math.ceil(y / 25))
+
+######################################################################################################
+'''
+#######################   Applying Morphological Transformations   ###################################
+
+# 1- Closing Operation to remove small holes in the image (black holes in the mask) 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
 # edges = cv2.Canny(pink_mask,100, 255)
 
+# 2- Erosion to flaten the surface of the coral 
+#    the surface isn't smooth in the linage between two pipes 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(25,1))
-horizontal = cv2.erode(mask,kernel)
+horizontal = cv2.erode(mask,kernel)               # horizontal Erosion to flaten Vertical surface 
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,25))
+vertical = cv2.erode(mask,kernel)                 # Vertical Erosion to flaten horizontal surface 
+######################################################################################################
+
 hor_contours, _ = cv2.findContours(horizontal,mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
 temp=[]
 hor_rectangles=[]
@@ -35,8 +55,7 @@ hor_contours = temp
 for i in hor_rectangles:
     cv2.rectangle(abstracted_coral, (i[0],int(i[1]+i[3]/2)),(i[0]+i[2],int(i[1]+i[3]/2)), (255,255,255), 2)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,25))
-vertical = cv2.erode(mask,kernel)
+
 ver_contours, _ = cv2.findContours(vertical,mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
 temp=[]
 ver_rectangles=[]
@@ -64,3 +83,4 @@ cv2.waitKey(0)
 #     for line in lines:
 #         x1, y1, x2, y2 = line[0]
 #         cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 1)
+'''
