63faf9e765356b8c7779228ab59c5f51fcb85b30
Ramez Noshy <RamezNoshy@gmail.com>
2019-12-06 16:18:29 +0200

    update depth handling

--- transect_line/transect_line.py
+++ transect_line/transect_line.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
-# todo determine slope and determine theta
-# todo determine xcostheta lletenen
-# todo distance  = max _i - x_j
+import math
+
 # video = cv2.VideoCapture('udpsrc port=8000 ! application/x-rtp,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 initial_state = True
-frame = cv2.imread('img_06.png')
+frame = cv2.imread('img_01.png')
 distance_threshold = 30
 orientation_threshold = 0.87
 
@@ -13,39 +12,33 @@ orientation_threshold = 0.87
 def get_distance(frame):
     frame = cv2.resize(frame, (600, 400))
     thresh = get_blue_mask(frame)
-    edges = cv2.Canny(thresh, 150, 250, 5)
-    lines = cv2.HoughLinesP(edges, 2, 1 * np.pi / 180, 45, minLineLength=60, maxLineGap=60)
-    mask = np.zeros(frame.shape, np.uint8)
-
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(mask, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    print(len(lines))
-    cv2.imshow("test", mask)
-    cv2.waitKey()
-
-    largest_two_contours = get_largest_two_contours(lines)
+    largest_two_contours = get_largest_two_contours(thresh)
+    m1 = midpoint(largest_two_contours[0])
+    m2 = midpoint(largest_two_contours[1])
+    # distance = math.hypot(m1, m2)
+    # print(distance)
+    distance = math.sqrt(pow(m1[0] - m2[0], 2) + pow(m1[1] - m2[1], 2))
     mask = np.zeros(frame.shape, np.uint8)
     cv2.drawContours(mask, largest_two_contours, -1, (0, 255, 0), 1)
     cv2.imshow("test", mask)
     cv2.waitKey()
-    return cv2.boundingRect(largest_two_contours[1])[0] - \
-           cv2.boundingRect(largest_two_contours[0])[0]
+    return distance
+
+
+def midpoint(cnt):
+    topmost = tuple(cnt[cnt[:, :, 1].argmin()][0])
+    bottommost = tuple(cnt[cnt[:, :, 1].argmax()][0])
+    mid = (bottommost[0] + topmost[0]) / 2, (bottommost[1] + topmost[1]) / 2
+    return mid
 
 
 def get_largest_two_contours(thresh):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow("thresh", thresh)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # contours = [c for c in contours if get_vertex_count(c) == 4]
-    largest_two_contours = sorted(contours, key=cv2.contourArea, reverse=True)[1:3]
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
-        print(f"h is{h}")
-        print(f"w is{w}")
-        print(f"ratio is{h/w}")
-    # x, y, w, h = cv2.boundingRect(c)
-    # return (h / w) > 4
-    # print(f"#of vertices {get_vertex_count(largest_two_contours)}")
+    contours = [c for c in contours if get_vertex_count(c) == 2]
+    largest_two_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:2]
     return largest_two_contours
 
 
@@ -60,7 +53,7 @@ def get_blue_mask(frame):
 
 def get_vertex_count(cnt):
     peri = cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, 0.01 * peri, True)
+    approx = cv2.approxPolyDP(cnt, 0.1 * peri, True)
     return len(approx)
 
 
@@ -77,9 +70,6 @@ def get_orientation(frame):
     return orientation1, orientation2
 
 
-print(get_distance(frame))
-cv2.waitKey()
-
 while True:
     # ret, frame = video.read()
     distance = get_distance(frame)
@@ -95,17 +85,17 @@ while True:
         elif distance_error < -distance_threshold:
             print(f"negative error -> enzl {distance_error}")
         else:
-            print(f"emshi odam yasta1")
+            print(f"dont change z yasta")
 
-        orientation_error = angles[0] - initial_angles[0]
+        orientation_error = angles[1] - initial_angles[1]
         if angles[0] > orientation_threshold:
-            print(f"lef shmal {orientation_error}")
+            print(f"lef ymeen {angles[1]}, {initial_angles[1]}, {orientation_error}")
         elif angles[0] < -orientation_threshold:
-            print(f"lef ymeen {orientation_error}")
+            print(f"lef shmal {angles[1]}, {initial_angles[1]}, {orientation_error}")
         else:
-            print(f"emshi odam yasta2")
+            print(f"dont change orientation yasta")
 
-    frame = cv2.imread('img_06.png')
+    frame = cv2.imread('img_07.png')
 
 # video.release()
 cv2.destroyAllWindows()
