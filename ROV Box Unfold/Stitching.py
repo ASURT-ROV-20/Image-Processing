220139cb28db6bc08064d25bd27eb33bbdd8c489
George Gabra Jr <george.gabra.jr@outlook.com>
2020-03-01 01:40:06 +0200

    Box Unfolding Mission Update
    -used morphological operation close to clean the mask for edge detection
    -used erosion to get edges for box and dilation to get edges for color square
    -got and separated the contour for the box and for the colors with a safety margin for cropping to get better end results
    -passed the color contours to the get_colors function for further processing
    -made proper debugging code and used the global __debug__ variable to enable and disable debugging
    -added code to calculate runtime

--- ROV Box Unfold/Stitching.py
+++ ROV Box Unfold/Stitching.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-
+import time
 
 def swap(i, j):
+    # Swaps two boxes images and their corresponding colors in their arrays
     global colors
     global imgs
     temp = imgs[i].copy()
@@ -14,13 +15,11 @@ def swap(i, j):
 
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
+    # initialize the dimensions of the image to be resized and grab the image size
     dim = None
     (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
+    # if both the width and height are None, then return the original image
     if width is None and height is None:
         return image
 
@@ -33,13 +32,12 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
     # otherwise, the height is None
     else:
-        # calculate the ratio of the width and construct the
-        # dimensions
+        # calculate the ratio of the width and construct the dimensions
         r = width / float(w)
         dim = (width, int(h * r))
 
     # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
+    resized = cv2.resize(image, dim, interpolation=inter)
 
     # return the resized image
     return resized
@@ -47,20 +45,59 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
 def edge_detect(frame):
     frame = cv2.GaussianBlur(frame, (3, 3), cv2.BORDER_DEFAULT)
+    # take a sample of the white color in the middle of the image, convert image to HSV for better ranges
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # take a sample of the white color in the middle of the image
     ht, wt, _ = hsv.shape
     roi = hsv[int(ht / 2) - int(ht / 30):int(ht / 2) + int(ht / 30), int(wt / 2) - int(wt / 30):int(wt / 2) + int(wt / 30)]
     h, s, v, _ = np.uint8(cv2.mean(roi))
     # mask the white color
-    lower = np.array([h-20, s-20, v-40])
-    upper = np.array([h+20, s+20, v+40])
+    lower = np.array([h-40, s-40, v-40])
+    upper = np.array([h+40, s+40, v+40])
     mask = cv2.inRange(hsv, lower, upper)
-    frame = cv2.Canny(mask, 100, 100)
-    blur5 = cv2.GaussianBlur(frame, (5, 5), cv2.BORDER_DEFAULT)
-    blur3 = cv2.GaussianBlur(frame, (3, 3), cv2.BORDER_DEFAULT)
-    result = blur5-blur3
-    return result
+    kernel = np.ones((5, 5), np.uint8)
+    outmask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=15)
+    colormask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=5)
+    outmask = cv2.erode(outmask, kernel, iterations=1)
+    colormask = cv2.dilate(colormask, kernel, iterations=2)
+
+    # DEBUG !!!!
+    if __debug__:
+        temp = frame.copy()
+        red = np.zeros(temp.shape, temp.dtype)
+        red[:, :] = (0, 0, 255)
+        maskoverlay = np.bitwise_and(red, outmask[:, :, np.newaxis])
+        cv2.addWeighted(temp, 0.5, maskoverlay, 1, 1, temp)
+        cv2.putText(temp, "Box Mask", (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 1, 1)
+        cv2.imshow("Debug", temp)
+        cv2.waitKey(0)
+        temp = frame.copy()
+        blue = np.zeros(temp.shape, temp.dtype)
+        blue[:, :] = (255, 0, 0)
+        maskoverlay = np.bitwise_and(blue, colormask[:, :, np.newaxis])
+        cv2.addWeighted(temp, 0.5, maskoverlay, 1, 1, temp)
+        cv2.putText(temp, "Colors Mask", (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 1, 1)
+        cv2.imshow("Debug", temp)
+        cv2.waitKey(0)
+    # DEBUG !!!!
+
+    # Get the mask edges
+    outeredge = cv2.Canny(outmask, 5, 5)
+    coloredges = cv2.Canny(colormask, 5, 5)
+
+    # DEBUG !!!!
+    if __debug__:
+        temp = np.bitwise_or(frame, outeredge[:, :, np.newaxis])
+        temp = np.bitwise_or(temp, coloredges[:, :, np.newaxis])
+        maskoverlay = np.bitwise_and(red, outeredge[:, :, np.newaxis])
+        maskoverlay2 = np.bitwise_and(blue, coloredges[:, :, np.newaxis])
+        cv2.addWeighted(temp, 0.5, maskoverlay, 1, 1, temp)
+        cv2.addWeighted(temp, 0.5, maskoverlay2, 1, 1, temp)
+        cv2.putText(temp, "Edges (Blue for colors Red for box)", (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 1, 1)
+        cv2.imshow("Debug", temp)
+        cv2.waitKey(0)
+    # DEBUG !!!!
+
+    return outeredge, coloredges
 
 
 def color_compare(color1, color2):
@@ -73,15 +110,15 @@ def color_compare(color1, color2):
         return False
 
 
-def get_colors(imgc):
+def get_colors(imgc, cnts):
     hc, wc, _ = imgc.shape
-
+    # sample colors from all sides of the box and its center
     roiTop = imgc[0:int(hc/20), int(wc/2) - int(wc/20):int(wc/2) + int(wc/20)]
     roiBottom = imgc[18*int(hc/20):hc, int(wc/2) - int(wc/20):int(wc/2) + int(wc/20)]
     roiLeft = imgc[int(hc/2) - int(hc/20):int(hc/2) + int(hc/20), 0:int(wc/20)]
     roiRight = imgc[int(hc/2) - int(hc/20):int(hc/2) + int(hc/20), 19*int(wc/20):wc]
     roiCenter = imgc[int(hc / 2) - int(hc / 30):int(hc / 2) + int(hc / 30), int(wc / 2) - int(wc / 30):int(wc / 2) + int(wc / 30)]
-
+    # get the mean color of the sampling area
     b, g, r, _ = np.uint8(cv2.mean(roiTop))
     top = [r, g, b]
     b, g, r, _ = np.uint8(cv2.mean(roiBottom))
@@ -98,6 +135,8 @@ def get_colors(imgc):
 imgs = []
 colors = []
 
+startTime = time.time()
+
 # load images from file
 for i in range(1, 6):
     img = cv2.imread("./Images/Box" + str(i) + ".jpg")
@@ -108,15 +147,41 @@ for i in range(0, 5):
     temp = imgs[i]
     imgs[i] = cv2.GaussianBlur(temp, (3, 3), cv2.BORDER_DEFAULT)
     # edges
-    edges = edge_detect(imgs[i])
-    # find the contours
-    cnts_t, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    outerEdge, colorsEdges = edge_detect(imgs[i])
+    # find the box contour
+    cnts_b, _ = cv2.findContours(outerEdge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # SORT
-    cnts_t = sorted(cnts_t, key=cv2.contourArea, reverse=True)
+    cnts_b = sorted(cnts_b, key=cv2.contourArea, reverse=True)
     # Get box contour
-    box_cnt = cnts_t[0]
-    epsilon = 0.1 * cv2.arcLength(box_cnt, True)
+    box_cnt = cnts_b[0]
+    # find colors contours
+    cnts_c, _ = cv2.findContours(colorsEdges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # remove box contour
+    cnts_c = sorted(cnts_c, key=cv2.contourArea, reverse=True)
+    cnts_c = cnts_c[2:]
+
+    # DEBUG !!!!
+    if __debug__:
+        temp = imgs[i].copy()
+        cv2.drawContours(temp, [box_cnt], -1, (0, 0, 255), 1)
+        cv2.drawContours(temp, cnts_c, -1, (255, 0, 0), 1)
+        cv2.putText(temp, "Contours (Blue for colors Red for box)", (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 1, 1)
+        cv2.imshow("Debug", temp)
+        cv2.waitKey(0)
+    # DEBUG !!!!
+
+    # Approximate contour to rectangle (hopefully)
+    epsilon = 0.07 * cv2.arcLength(box_cnt, True)
     box_cnt = cv2.approxPolyDP(box_cnt, epsilon, True)
+
+    # DEBUG !!!!
+    if __debug__:
+        temp = imgs[i].copy()
+        cv2.drawContours(temp, box_cnt, -1, (0, 255, 0), 5)
+        cv2.putText(temp, "Box Approximation", (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 1, 1)
+        cv2.imshow("Debug", temp)
+        cv2.waitKey(0)
+    # DEBUG !!!!
 
     # get edge points for perspective warp correction
     pts = box_cnt.reshape(4, 2)
@@ -139,8 +204,8 @@ for i in range(0, 5):
     # Get and apply perspective warp correction
     M = cv2.getPerspectiveTransform(rect, dst)
     imgs[i] = cv2.warpPerspective(imgs[i], M, (maxWidth, maxHeight))
-
-    colors.append(get_colors(imgs[i]))
+    # Get and append box colors to their array
+    colors.append(get_colors(imgs[i], cnts_c))
 
 
 # check bottom color to be not white (top side) and puts it in the beginning of the array
@@ -169,6 +234,7 @@ for i in range(2, 5):
         swap(2, i)
         break
 
+# gets plate on the side of the third plate and puts it in fourth place
 for i in range(3, 5):
     print("Comparing box 2 right, color (" + str(colors[2][3]) + ") with box " + str(i) + " left color (" + str(colors[i][2]) + ")")
     current = colors[2][3]  # third side right color
@@ -176,6 +242,8 @@ for i in range(3, 5):
         print("found it")
         swap(3, i)
         break
+
+# The fifth plate is in its correct position as t is the only remaining one
 
 # Correct image heights to match
 for i in range(0, 5):
@@ -222,4 +290,5 @@ h5, w5, _ = imgs[4].shape
 final[heightf-h5:heightf, delx:delx+w5] = imgs[4]
 
 cv2.imshow("Final", final)
+print(" ---- %s seconds ----" % (time.time()-startTime))
 cv2.waitKey(0)
